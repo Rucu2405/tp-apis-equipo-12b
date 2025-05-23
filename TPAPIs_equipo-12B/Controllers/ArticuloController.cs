493e26455987bac2acd6ac865234829e7d8ad7f2
Rucu2405 <agustinleonelzuloaga@gmail.com>
2025-05-23 19:17:24 -0300

    Se agregó funcionalidad para agregar artículo y eliminar artículo con mensajes de éxito, cambiando el void del metodo por el httpresponse, además de agregar el dto de artículo. Falta agregar imagen y eliminar imagen.

--- TPAPIs_equipo-12B/Controllers/ArticuloController.cs
+++ TPAPIs_equipo-12B/Controllers/ArticuloController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using System.Web.Http.Results;
 using dominio;
 using negocio;
+using TPAPIs_equipo_12B.Models;
 
 namespace TPAPIs_equipo_12B.Controllers
 {
@@ -18,7 +19,7 @@ namespace TPAPIs_equipo_12B.Controllers
         //Obtener listado de artículos...
         // GET: api/Articulo
         public IEnumerable<Articulo> Get()
-        {           
+        {
             return negocio.ListarArticulos();
         }
 
@@ -27,13 +28,24 @@ namespace TPAPIs_equipo_12B.Controllers
         public Articulo Get(int id)
         {
             List<Articulo> listado = negocio.ListarArticulos();
-            return listado.Find(x=> x.Id == id);    
-
+            return listado.Find(x => x.Id == id);
         }
 
+        //Agregar artículo con mensaje de éxito...
         // POST: api/Articulo
-        public void Post([FromBody]Articulo art)
+        public HttpResponseMessage Post([FromBody] ArticuloDto dto)
         {
+            Articulo nuevo = new Articulo();
+            nuevo.Codigo = dto.Codigo;
+            nuevo.Nombre = dto.Nombre;
+            nuevo.Descripcion = dto.Descripcion;
+            nuevo.Marca = new Marca { Id = dto.IdMarca };
+            nuevo.Categoria = new Categoria { Id = dto.IdCategoria };
+            nuevo.Precio = dto.Precio;
+            negocio.agregar(nuevo);
+            return Request.CreateResponse(HttpStatusCode.OK, "¡Artículo agregado correctamente!");
+
+            //acá iría el agregar imágenes
         }
 
         // PUT: api/Articulo/5
@@ -41,9 +53,14 @@ namespace TPAPIs_equipo_12B.Controllers
         {
         }
 
+        //Eliminación física 
         // DELETE: api/Articulo/5
-        public void Delete(int id)
+        public HttpResponseMessage Delete(int id)
         {
+            negocio.Eliminar(id);
+            return Request.CreateResponse(HttpStatusCode.OK, "Artículo eliminado con éxito.");
+
+            //acá iría el eliminar imágenes
         }
     }
 }
