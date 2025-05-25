ff8c27dea3fc4a9d8b1f2819ce11aa30f7b017d5
karinatorrilla <karinatorrilla1@gmail.com>
2025-05-25 11:50:52 -0300

    Validaciones en crear y modificar, que los campos tengan formato valido al enviar, y también se agrega como campos obligatorios en nuestra regla de negocio: Codigo,Nombre y Precio

--- TPAPIs_equipo-12B/Controllers/ArticuloController.cs
+++ TPAPIs_equipo-12B/Controllers/ArticuloController.cs
@@ -55,7 +55,7 @@ namespace TPAPIs_equipo_12B.Controllers
                 return Request.CreateResponse(HttpStatusCode.OK, articulo);
 
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 return Request.CreateResponse(HttpStatusCode.NotFound, "El articulo Nro: " + id + ", no existe");
 
@@ -67,33 +67,78 @@ namespace TPAPIs_equipo_12B.Controllers
         // POST: api/Articulo
         public HttpResponseMessage Post([FromBody] ArticuloDto dto)
         {
-            //Valida que el ID de la marca y categoria exista
-            Marca marcaExistente = marcaNegocio.ListarMarca().Find(x => x.Id == dto.IdMarca);
-            Categoria categoriaExistente = categoriaNegocio.ListarCategoria().Find(x => x.Id == dto.IdCategoria);
-
-
-            if (marcaExistente == null)
+            try
             {
-                return Request.CreateResponse(HttpStatusCode.NotFound, "La marca Nro: " + dto.IdMarca + " para modificar, no existe");
-            }
+                //Valida que el dto sea correcto
+                if (dto == null)
+                {
+                    if (!ModelState.IsValid)
+                    {
+                        foreach (var state in ModelState)
+                        {
+                            foreach (var error in state.Value.Errors)
+                            {
+                                ///return Request.CreateResponse(HttpStatusCode.BadRequest, "aaaaERRORRRR----->." + state.Value.Errors);
+                                ///return Request.CreateResponse(HttpStatusCode.BadRequest, "aaaaERRORRRR22----->." + state.Key);
+                                //Capturamos el nombre del campo que tira error
+                                string campoInvalido = state.Key.Contains(".") ? state.Key.Split('.').Last() : state.Key;
+                                return Request.CreateResponse(HttpStatusCode.BadRequest, "Debe ingresar un formato válido en el campo: " + campoInvalido);
+                            }
 
-            if (categoriaExistente == null)
+                        }
+                            
+                    }
+
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Datos de artículo inválidos. Asegúrese de que el formato  sea el correcto.");
+                }
+
+                //Valida que el ID de la marca y categoria exista
+                Marca marcaExistente = marcaNegocio.ListarMarca().Find(x => x.Id == dto.IdMarca);
+                Categoria categoriaExistente = categoriaNegocio.ListarCategoria().Find(x => x.Id == dto.IdCategoria);
+
+
+                if (marcaExistente == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "La marca Nro: " + dto.IdMarca + " para agregar, no existe");
+                }
+
+                if (categoriaExistente == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "El categoría Nro: " + dto.IdCategoria + " para agregar, no existe");
+                }
+
+                //Validamos como campos obligatorios Codigo,Nombre y Precio:
+                if (string.IsNullOrWhiteSpace(dto.Codigo))
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "El código del artículo es obligatorio.");
+                }
+                if (string.IsNullOrWhiteSpace(dto.Nombre))
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "El nombre del artículo es obligatorio.");
+                }
+                if (dto.Precio <= 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "El precio del artículo debe ser mayor a 0.");
+                }
+
+                Articulo nuevo = new Articulo();
+                nuevo.Codigo = dto.Codigo;
+                nuevo.Nombre = dto.Nombre;
+                nuevo.Descripcion = dto.Descripcion;
+                nuevo.Marca = new Marca { Id = dto.IdMarca };
+                nuevo.Categoria = new Categoria { Id = dto.IdCategoria };
+                nuevo.Precio = dto.Precio;
+                negocio.agregar(nuevo);
+                return Request.CreateResponse(HttpStatusCode.Created, "¡Artículo agregado correctamente!");
+
+            }
+            catch (Exception ex)
             {
-                return Request.CreateResponse(HttpStatusCode.NotFound, "El categoría Nro: " + dto.IdCategoria + " para modificar, no existe");
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Ocurrió un error inesperado al agregar el artículo: " + ex.Message);
             }
-
-
-            Articulo nuevo = new Articulo();
-            nuevo.Codigo = dto.Codigo;
-            nuevo.Nombre = dto.Nombre;
-            nuevo.Descripcion = dto.Descripcion;
-            nuevo.Marca = new Marca { Id = dto.IdMarca };
-            nuevo.Categoria = new Categoria { Id = dto.IdCategoria };
-            nuevo.Precio = dto.Precio;
-            negocio.agregar(nuevo);
-            return Request.CreateResponse(HttpStatusCode.OK, "¡Artículo agregado correctamente!");
-
-            //acá iría el agregar imágenes
+            
+            
         }
 
         /// Para agregar imagenes a los articulos
@@ -113,7 +158,7 @@ namespace TPAPIs_equipo_12B.Controllers
                 negocio.AgregarImagenes(id, dto);
                 return Request.CreateResponse(HttpStatusCode.OK, "¡Imagenes agregadas correctamente al Articulo Nro: " + id + "! ");
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 return Request.CreateResponse(HttpStatusCode.InternalServerError, "Error inesperado");
             }
@@ -128,6 +173,31 @@ namespace TPAPIs_equipo_12B.Controllers
         {
             try
             {
+                //Valida que el dto sea correcto
+                if (dto == null)
+                {
+                    if (!ModelState.IsValid)
+                    {
+                        foreach (var state in ModelState)
+                        {
+                            foreach (var error in state.Value.Errors)
+                            {
+                                ///return Request.CreateResponse(HttpStatusCode.BadRequest, "aaaaERRORRRR----->." + state.Value.Errors);
+                                ///return Request.CreateResponse(HttpStatusCode.BadRequest, "aaaaERRORRRR22----->." + state.Key);
+                                //Capturamos el nombre del campo que tira error
+                                string campoInvalido = state.Key.Contains(".") ? state.Key.Split('.').Last() : state.Key;
+                                return Request.CreateResponse(HttpStatusCode.BadRequest, "Debe ingresar un formato válido en el campo: " + campoInvalido);
+                            }
+
+                        }
+
+                    }
+
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Datos de artículo inválidos. Asegúrese de que el formato  sea el correcto.");
+                }
+
+
+
                 //Valida que el ID del articulo, marca y categoria exista
                 Articulo articuloExistente = negocio.buscarArticulo(id);
                 Marca marcaExistente = marcaNegocio.ListarMarca().Find(x => x.Id == dto.IdMarca);
@@ -147,6 +217,21 @@ namespace TPAPIs_equipo_12B.Controllers
                 {
                     return Request.CreateResponse(HttpStatusCode.NotFound, "El categoría Nro: " + dto.IdCategoria + " para modificar, no existe");
                 }
+
+                //Validamos como campos obligatorios Codigo,Nombre y Precio:
+                if (string.IsNullOrWhiteSpace(dto.Codigo))
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "El código del artículo es obligatorio.");
+                }
+                if (string.IsNullOrWhiteSpace(dto.Nombre))
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "El nombre del artículo es obligatorio.");
+                }
+                if (dto.Precio <= 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "El precio del artículo debe ser mayor a 0.");
+                }
+
 
                 Articulo nuevo = new Articulo();
                 nuevo.Codigo = dto.Codigo;
